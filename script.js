e347f797205b007c6856bb61656b366f94224c6d
mgueze2 <matthias.guezennec-loviton@univ-paris-saclay.fr>
2024-12-09 16:35:30 +0100

    premiere vers.

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();  // Empêche l'envoi du formulaire pour validation
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    
+    if (name && email) {
+      alert('Merci pour votre message, ' + name + '!');
+    } else {
+      alert('Veuillez remplir tous les champs.');
+    }
+  });
+  
